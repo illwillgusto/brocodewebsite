a1a19b4f2cac453687e058b33bce528105268baa
Will Walton <will@endless3dge.com>
2024-05-09 18:03:11 -0500

    Update Shoes.jsx
    shoe variables added to the list items

--- my-newreact-app/src/Shoes.jsx
+++ my-newreact-app/src/Shoes.jsx
@@ -1,16 +1,14 @@
-
 function Shoes() {
+	const shoe1 = 'Foamposites';
+	const shoe2 = 'Jordan1s';
 
-  const shoe1 = "Foamposites";
-  const shoe2 = "Jordan1s";
-
-  return(
-    <ul>
-      <li>Air Force 1</li>
-      <li></li>
-      <li></li>
-    </ul>
-  );
+	return (
+		<ul>
+			<li>Air Force 1</li>
+			<li>{shoe1}</li>
+			<li>{shoe2}</li>
+		</ul>
+	);
 }
 
-export default Shoes
+export default Shoes;
