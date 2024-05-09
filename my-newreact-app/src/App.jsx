3d5312c9231fea84705b289e1e76a57cee17a34c
Will Walton <will@endless3dge.com>
2024-05-09 18:09:32 -0500

    card component added
    created the card component and added 3 elements
    an img element
    a header2 element and a para element with a short description
    
    added the card component to the app component
    removed the header and shoe component from the app component

--- my-newreact-app/src/App.jsx
+++ my-newreact-app/src/App.jsx
@@ -1,12 +1,11 @@
-import Header from './Header.jsx';
 import Footer from './Footer.jsx';
-import Shoes from './Shoes.jsx';
+import Card from './Card.jsx'
 
 function App() {
 	return (
 		<>
-			<Header />
-			<Shoes />
+
+			<Card/>
 			<Footer />
 		</>
 	);
---
+++ my-newreact-app/src/Card.jsx
@@ -0,0 +1,12 @@
+
+function Card() {
+  return(
+    <div className="card">
+      <img></img>
+      <h2>Will</h2>
+      <p>I am a Solutions Engineer and play video games</p>
+    </div>
+  );
+}
+
+export default Card
