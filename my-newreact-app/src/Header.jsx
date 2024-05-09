0807ad2f7856518b3092aa47751be64aacbafad1
Will Walton <will@endless3dge.com>
2024-05-09 17:48:45 -0500

    Update Header.jsx
    changed all listed items to hyperlinks
    the links aren't current going anywhere

--- my-newreact-app/src/Header.jsx
+++ my-newreact-app/src/Header.jsx
@@ -6,10 +6,10 @@ function Header() {
         <h1>My Website</h1>
         <nav>
           <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Services</li>
-            <li>Contact</li>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Contact</a></li>
           </ul>
         </nav>
       </header>
