00f5e8b37570bf2880549492e4006b2a08666a1d
Will Walton <will@endless3dge.com>
2024-05-09 18:25:32 -0500

    Update Card.jsx
    className added for the card description

--- my-newreact-app/src/Card.jsx
+++ my-newreact-app/src/Card.jsx
@@ -5,7 +5,7 @@ function Card() {
 		<div className='card'>
 			<img className='card-image' src={profilePic} alt='profile picture'></img>
 			<h2 className='card-title'>Will</h2>
-			<p>I am a Solutions Engineer and play video games</p>
+			<p className='card-description'>I am a Solutions Engineer and play video games</p>
 		</div>
 	);
 }
