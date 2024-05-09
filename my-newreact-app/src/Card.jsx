7d513b132acd424d6360c70d2b9c12da8197b5eb
Will Walton <will@endless3dge.com>
2024-05-09 18:19:55 -0500

    Update Card.jsx
    className added to the card image

--- my-newreact-app/src/Card.jsx
+++ my-newreact-app/src/Card.jsx
@@ -3,7 +3,7 @@ import profilePic from './assets/profile.jpeg'
 function Card() {
 	return (
 		<div className='card'>
-			<img src={profilePic} alt='profile picture'></img>
+			<img className='card-image' src={profilePic} alt='profile picture'></img>
 			<h2>Will</h2>
 			<p>I am a Solutions Engineer and play video games</p>
 		</div>
