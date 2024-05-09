7eb4093b868ae3ecaec6d4b2c009551ef50475fb
Will Walton <will@endless3dge.com>
2024-05-09 17:55:22 -0500

    Update Footer.jsx
    added js with the new date and getfullyear method so the current year is displayed after the copyright symbol

--- my-newreact-app/src/Footer.jsx
+++ my-newreact-app/src/Footer.jsx
@@ -2,9 +2,9 @@
 function Footer() {
   return(
     <footer>
-      <p>&copy; Your website name</p>
+      <p>&copy; {new Date().getFullYear()} Your website name</p>
     </footer>
-  )
+  );
 }
 
 export default Footer
