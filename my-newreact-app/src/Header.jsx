4b5077ff2d83c81f413416c4c7a31910936bf420
Will Walton <will@endless3dge.com>
2024-05-09 17:46:53 -0500

    Update Header.jsx
    ul element added with 4 li elements

--- my-newreact-app/src/Header.jsx
+++ my-newreact-app/src/Header.jsx
@@ -4,6 +4,14 @@ function Header() {
     return(
       <header>
         <h1>My Website</h1>
+        <nav>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Services</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
       </header>
     );
 }
