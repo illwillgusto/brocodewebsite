c065360872fb358f8f1d8efbb8c42287b16cc048
Will Walton <will@endless3dge.com>
2024-05-09 18:16:20 -0500

    profile img added
    profile image added
    imported the img from the assets folder
    applied the img to the src of the img

--- my-newreact-app/src/Card.jsx
+++ my-newreact-app/src/Card.jsx
@@ -1,12 +1,13 @@
+import profilePic from './assets/profile.jpeg'
 
 function Card() {
-  return(
-    <div className="card">
-      <img></img>
-      <h2>Will</h2>
-      <p>I am a Solutions Engineer and play video games</p>
-    </div>
-  );
+	return (
+		<div className='card'>
+			<img src={profilePic} alt='profile picture'></img>
+			<h2>Will</h2>
+			<p>I am a Solutions Engineer and play video games</p>
+		</div>
+	);
 }
 
-export default Card
+export default Card;
