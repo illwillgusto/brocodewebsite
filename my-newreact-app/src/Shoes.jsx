c4bedca1c80b29c1dc44fdff7133dd1072e99590
Will Walton <will@endless3dge.com>
2024-05-09 18:00:09 -0500

    shoe component created
    shoe component created

---
+++ my-newreact-app/src/Shoes.jsx
@@ -0,0 +1,16 @@
+
+function Shoes() {
+
+  const shoe1 = "Foamposites";
+  const shoe2 = "Jordan1s";
+
+  return(
+    <ul>
+      <li>Air Force 1</li>
+      <li></li>
+      <li></li>
+    </ul>
+  );
+}
+
+export default Shoes
