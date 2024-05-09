fa12baa45e2af6782baf5dc9b8b663867ba1feb9
Will Walton <will@endless3dge.com>
2024-05-09 18:01:10 -0500

    Update App.jsx
    added the shoe component to the app component

--- my-newreact-app/src/App.jsx
+++ my-newreact-app/src/App.jsx
@@ -1,10 +1,12 @@
 import Header from './Header.jsx';
 import Footer from './Footer.jsx';
+import Shoes from './Shoes.jsx';
 
 function App() {
 	return (
 		<>
 			<Header />
+      <Shoes/>
 			<Footer />
 		</>
 	);
