2349254422c54d678f5c9c85b9377d2878757386
Will Walton <will@endless3dge.com>
2024-05-09 17:53:58 -0500

    Update App.jsx
    added the Footer component to the app component
    used the fragment element to wrap the components

--- my-newreact-app/src/App.jsx
+++ my-newreact-app/src/App.jsx
@@ -1,10 +1,13 @@
-import Header from "./Header.jsx"
+import Header from './Header.jsx';
+import Footer from './Footer.jsx';
 
 function App() {
-
-  return(
-    <Header/>
-  );
+	return (
+		<>
+			<Header />
+			<Footer />
+		</>
+	);
 }
 
-export default App
+export default App;
